c9d4229d8fc3cc6b60e28513e93ef39309648629
sharathsatheesh <poikayilsharath@gmail.com>
2016-01-30 10:20:05 +0800

    Update with comments

--- cachematrix.R
+++ cachematrix.R
@@ -3,15 +3,15 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function(y) {
-    x <<- y
+makeCacheMatrix <- function(x = matrix()) {## There are four functions in this environment, and ALSO the x and inv objects (a consequence of using <<-). And the get and getInverse functions only fetch these from their enclosing environment.
+  inv <- NULL ## set inverse flag to null inside functions scope
+  set <- function(y) { ## define a function set that sets the inv flag to null
+    x <<- y ##the assignment of y to x is valid in parent scope
     inv <<- NULL
   }
-  get <- function() x
-  setinverse <- function(inverse) inv <<- inverse
-  getinverse <- function() inv
+  get <- function() x 
+  setinverse <- function(inverse) inv <<- solve(x) ## set inverse of matrix
+  getinverse <- function() inv 
   list(set = set,
        get = get,
        setinverse = setinverse,
@@ -19,7 +19,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## this function checks the inv flag to see if the inverse is cached. if its cached it fetches the cached matrix, if not inverse is calculated
 
 cacheSolve <- function(x, ...) {
   ## return the inverse matrix of x
